5f3449c4e73c6160d376f39a6990dc187c4d78a5
Nicolas Gutierrez <yefry-nicolas123@hotmail.com>
2024-02-26 14:54:31 -0500

    working

---
+++ js/client.js
@@ -0,0 +1,120 @@
+const { createApp, ref, computed } = Vue;
+
+const SERVICE_URL = "http://localhost:3000/cars";
+
+const app = createApp({
+  data() {
+    const totalDonations = 0;
+    const value = 0;
+    const message = "";
+    const car = "renault";
+    const license_plate = "duc-851";
+    const color = "gris"; // Remove the unnecessary line continuation character
+    const license_Delete = "";
+    const cars = [];
+    return {
+      totalDonations,
+      value,
+      message,
+      car,
+      license_plate,
+      color,
+      license_Delete,
+      cars
+    };
+  },
+  methods: {
+
+    async sendDonation() {
+      this.message = "Entra a sendDonation";
+      const response = await fetch("http://localhost:3000/donations", {
+        method: "POST",
+        body: JSON.stringify({ donationAmount: this.value }),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      });
+      if (response.status == 200) {
+        this.message =
+          "El valor ha sido actualizado, presione refrescar para ver el nuevo valor.";
+      } else {
+        this.message =
+          "Ha ocurrido un error en el servidor y la donación no han sido registrados.";
+      }
+    },
+
+    async getCars() {
+      const response = await fetch("http://localhost:3000/carst");
+    
+      if (response.ok) {
+        const cars = await response.json();
+        console.log("Received cars:", cars);
+      } else {
+        console.log("Error:", response.status);
+      }
+    },
+   
+    async sendCarData() {
+      const carData = {
+        brand: this.car,
+        license_plate: this.license_plate,
+        color: this.color,
+      };
+      const response = await fetch(SERVICE_URL, {
+        method: "POST",
+        body: JSON.stringify(carData),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      });
+      if (response.status == 200) {
+        this.message = "El carro ha sido registrado.";
+      } else {
+        this.message = "Ha ocurrido un error en el servidor.";
+      }
+    },
+
+    async retireCar() {
+      const license_Delete = {
+        license_delete: this.license_Delete,
+      };
+      const response = await fetch(SERVICE_URL, {
+        method: "PATCH",
+        body: JSON.stringify(license_Delete),
+        //body: JSON.stringify({ license_Delete: this.license_Delete }),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      });
+    
+      if (response.ok) {
+        const car = await response.json();
+        console.log("Car retired:", car);
+      } else {
+        console.log("Error:", response.status);
+      }
+    },
+
+    async refreshDonations() {
+      const response = await fetch("http://localhost:3000/donations");
+      const jsonResponse = await response.json();
+      const newDonationsValue = jsonResponse.total;
+      this.totalDonations = newDonationsValue;
+      this.message = "Entra a refresh donations";
+    },
+
+    async getCarsFromServer() {
+      try {
+        const response = await fetch(SERVICE_URL);
+    
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    
+        this.cars = await response.json();
+
+    
+        console.log('Cars from server:', cars);
+      } catch (error) {
+        console.log('There was a problem with the fetch operation: ', error);
+      }
+    }
+    
+  },
+});
+
+app.mount("#app");

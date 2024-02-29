8be553c5269abfbbfe4e32ccdd5a424289eb3b14
Nicolas Gutierrez <yefry-nicolas123@hotmail.com>
2024-02-29 13:05:05 -0500

    mejora de interfaz

--- js/client.js
+++ js/client.js
@@ -1,6 +1,6 @@
 const { createApp, ref, computed } = Vue;
 
-const SERVICE_URL = "http://localhost:3000/cars";
+const SERVICE_URL = "http://192.168.80.18:3000/cars";
 
 const app = createApp({
   data() {
